3496b2b49e28df73741b6ca753d4012f6fcab3a1
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-21 00:37:35 +0100

    todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-
 function App() {
 
-
   return (
-    <>
-      <div className=" mx-auto">Hello Folarin</div>
-    </>
+
+    <div className="max-w-3xl mx-auto w-full">
+      <div className="mx-auto text-2xl font-mono pt-11">Todo List</div>
+    </div>
+
   )
 }
 
