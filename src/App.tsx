9a61cf0d57df12b9e18b6e4e42014176e0605675
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-21 00:47:14 +0100

    todotop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Todotop from "./Components/Todotop"
+
+
 function App() {
 
   return (
 
     <div className="max-w-3xl mx-auto w-full">
       <div className="mx-auto text-2xl font-mono pt-11">Todo List</div>
+      <Todotop />
     </div>
 
   )
--- src/Components/Todotop.tsx
+++ src/Components/Todotop.tsx
@@ -2,7 +2,7 @@
 const Todotop = () => {
   return (
     <div>
-      
+      <input type="text" className="py-16 px-12"/>
     </div>
   )
 }
