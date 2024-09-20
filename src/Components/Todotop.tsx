840bd5ac68885037b98929611b08fa9308ff6a0b
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-21 00:49:22 +0100

    solid

--- src/Components/Todotop.tsx
+++ src/Components/Todotop.tsx
@@ -2,7 +2,7 @@
 const Todotop = () => {
   return (
     <div>
-      <input type="text" className="py-16 px-12"/>
+      <input type="text" className=" border border-solid my-4"/>
     </div>
   )
 }
