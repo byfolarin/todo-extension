e03de76f1f4afc71b9e9071c84836498346fb441
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-21 01:13:09 +0100

    flex

--- src/Components/Todotop.tsx
+++ src/Components/Todotop.tsx
@@ -1,9 +1,9 @@
 
 const Todotop = () => {
   return (
-    
-    <div className="flex gap-4 mt-4">       
-            <input type="text" className=" border border-solid py-2 rounded-lg"/>
+
+    <div className="flex gap-2 mt-4">       
+            <input type="text" className=" border border-solid py-2 rounded-lg px-4"/>
             <button className="text-white bg-black text-sm rounded-lg px-6">Add</button>   
     </div>
   )
