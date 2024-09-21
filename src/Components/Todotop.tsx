f34a3258e572b9918dfd3807aef53e44429f8526
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-21 01:50:33 +0100

    up-to-date

--- src/Components/Todotop.tsx
+++ src/Components/Todotop.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react"
+// import { useState } from "react"
 
-const Todotop: React.FC = () => { // Specify the component type
+const Todotop = () => {
 
-    const [display, setDisplay] = useState<string>(""); // Add type for useState
+    // const [display, setDisplay] = useState
 
-    function handleClick(): void { // Specify return type
-        // Handle click logic here
-    }
+
+// function handleClick (){
+
+// }
 
   return (
     <div className="flex gap-2 mt-4">       
-            <input type="text" className="border border-solid py-2 rounded-lg px-4" />
-            <button className="text-white bg-black text-sm rounded-lg px-6" onClick={handleClick}>Add</button> {/* Added onClick handler */}
+            <input type="text"  className=" border border-solid py-2 rounded-lg px-4"/>
+            <button className="text-white bg-black text-sm rounded-lg px-6">Add</button>   
     </div>
   )
 }
