373ea7ef3a02fde01bd7f05a83f4987cdceb3174
Folarin Folarin <thefolafolarin@gmail.com>
2024-09-21 01:00:43 +0100

    typescript

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
     <div className="max-w-3xl mx-auto w-full">
       <div className="mx-auto text-2xl font-mono pt-11">Todo List</div>
-      <Todotop />
+         <Todotop />
     </div>
 
   )
--- src/Components/Todotop.tsx
+++ src/Components/Todotop.tsx
@@ -1,8 +1,9 @@
 
 const Todotop = () => {
   return (
-    <div>
+    <div className="flex">
       <input type="text" className=" border border-solid my-4"/>
+      <button className="text-white bg-black text-base  px-4">Add</button>
     </div>
   )
 }
